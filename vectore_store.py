7d657912afa45d2c0b9339f0921c7ceb34bcd205
sharjeel <sharjeelmasood10b15@gmail.com>
2025-06-06 18:59:09 +0900

    mod vecstore

--- vectore_store.py
+++ vectore_store.py
@@ -2,38 +2,70 @@ import re
 import chromadb
 from chromadb.utils import embedding_functions
 
+from PyPDF2 import PdfReader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class VectorStore:
     def __init__(self, data_path: str = "./data"):
         self.data_path = data_path
-        self.documents = []
 
         chroma_client = chromadb.PersistentClient(path = self.data_path)
         embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name = "all-MiniLM-L6-v2")
         self.collection = chroma_client.get_or_create_collection(name = "products", embedding_function = embedding_function)
 
-    def load_documents(self, file_path: str):
-        """Load documents from the specified file."""
+    def ingest_documents(self, file_path: str, chunk_size: int = 500, chunk_overlap: int = 100):
+        """Ingest documents from the specified file."""
         if file_path.endswith('.pdf'):
-            self.load_pdf()
+            self.prepare_pdf(file_path, chunk_size, chunk_overlap)
         else:
             raise ValueError("Unsupported file format. Only PDF files are supported.")
 
-    def load_pdf(self, PDF_PATH: str):
-        loader = PyPDFLoader(PDF_PATH)
-        documents = loader.load()
+
+    def load_pdf(self, pdf_file: str, chunk_size: int, chunk_overlap: int):
+        reader = PdfReader(pdf_file)    
+        documents = {}
+        for page_no in range(len(reader.pages)):        
+            page = reader.pages[page_no]
+            text = page.extract_text() 
+            text_chunks = self.get_chunks(text, chunk_size, chunk_overlap)
+            documents[page_no] = text_chunks
+
+        return documents
+
+    def get_chunks(self, text: str, chunk_size: int, chunk_overlap: int) -> list:
 
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,      # Max characters in a chunk
-            chunk_overlap=200,    # Overlap between chunks to maintain context
-            length_function=len,
-            is_separator_regex=False,
+            chunk_size = chunk_size,      # Max characters in a chunk
+            chunk_overlap = chunk_overlap,    # Overlap between chunks to maintain context
+            length_function = len,
+            is_separator_regex = False,
         )
-        chunks = text_splitter.split_documents(documents)
+        chunks = text_splitter.split_documents(text)
+        return chunks
 
+    def add_metadata(self, docs, file_name):
+        docs_strings = []  
+        ids = []  
+        metadatas = []  
+        id = 0  
+            
+        for page_no in docs.keys():
+            for doc in docs[page_no]:        
+                docs_strings.append(doc)                        
+                metadatas.append({'page_no': page_no,"file_name": file_name})
+                ids.append(id)
+                id += 1
 
-    def get_documents(self):
-        """Return the loaded documents."""
-        return self.documents
+            self.collection.add(
+                ids=[str(id) for id in ids],  
+                documents=docs_strings,  
+                metadatas=metadatas,  
+            )
+    
+    def prepare_pdf(self, pdf_file: str, chunk_size: int, chunk_overlap: int):
+        """Prepare the PDF file for vector storage."""
+        docs = self.load_pdf(pdf_file, chunk_size, chunk_overlap)
+        self.add_metadata(docs, pdf_file)
+        
+
