730bfbf7503d26d24437ee90f1d6e6b501aaf59e
sharjeel <sharjeelmasood10b15@gmail.com>
2025-06-08 21:06:19 +0900

    agent ingestion

--- main_agent.py
+++ main_agent.py
@@ -1,10 +1,31 @@
 from openai import OpenAI
 
+from vectore_store import VectorStore
+
 
 class Agent:
     def __init__(self, model, API_KEY):
         self.model = model
         self.llm = OpenAI(api_key = API_KEY)
+        self.vector_store = VectorStore(API_KEY = API_KEY)
+        
+        self.chunk_size = 600
+        self.chunk_overlap = 100
+
+    def Ingest(self, file_path, image_paths = None):
+        if file_path != None and image_paths == None:
+            self.vector_store.ingest_pdf(
+                pdf_file = file_path,
+                chunk_size = self.chunk_size,
+                chunk_overlap = self.chunk_overlap
+            )
+        elif image_paths != None and file_path != None:
+            self.vector_store.ingest_text_images(
+                pdf_file = file_path,
+                chunk_size = self.chunk_size,
+                chunk_overlap = self.chunk_overlap,
+                image_dirs = image_paths
+            )
 
     def generate_response(self, prompt, context):
         input_text = (
