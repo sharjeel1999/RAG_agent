d6f65f54f53c66c891a291b66f7b25747eac9aa2
sharjeel <sharjeelmasood10b15@gmail.com>
2025-06-10 01:43:15 +0900

    tools

--- toolbox.py
+++ toolbox.py
@@ -1,11 +1,14 @@
-from langchain_community.tools.tavily_research import TavilySearchResults
+# from langchain_community.tools.tavily_research import TavilySearchResults
+from langchain_community.tools import DuckDuckGoSearchRun
 
 class ToolBox:
     def __init__(self, API_KEY: str):
         self.API_KEY = API_KEY
-        self.tavily_search = TavilySearchResults(api_key = API_KEY)
+        self.duckduckgo_runner = DuckDuckGoSearchRun()
 
     def search(self, query: str, num_results: int = 5):
         return self.tavily_search.run(query = query, num_results = num_results)
     
-    
+
+tools = ToolBox(API_KEY = "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")
+print(tools.tavily_search)
